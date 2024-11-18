071d8592b8fa368595b631ebd83a3367af408839
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-11-18 20:50:36 +0500

    Basic Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
