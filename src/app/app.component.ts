749b0f483f9c87c31c8759d937db88f036cb31b4
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-11-19 14:35:46 +0500

    Introduction to service in Angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basic_routing';
+  memberData = [
+    { name: "asadUllah", email: "asad@test.com" },
+    { name: "Bilal", email: "bilal@test.com" },
+    { name: "Mahrukh", email: "mahrukh@test.com" },
+    { name: "saqlain", email: "saqlain@test.com" }
+  ]
 }
